d0b6d3569feaaecccc12fc5e5d5ac4b040c99fe4
TonyFeng <sgtonyn@gmail.com>
2024-02-22 20:19:09 +0700

    Add: debounce and throttle functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 const $ = (selector) => document.querySelector(selector);
+const log = console.log;
 const initApp = () => {
-  const button = $("button");
-  // click event
-  button.addEventListener("click", debounce(clickOrder, 2000));
+  const button1 = $("#button1");
+  const button2 = $("#button2");
+  // click event vs debounce
+  button1.addEventListener("click", debounce(clickGetUser, 2000));
+  // input event vs throttle
+  button2.addEventListener("click", throttle(clickGetUser, 3000));
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
-const clickOrder = async () => {
+
+// click event handler
+const clickGetUser = async () => {
   const name = $("#name");
   const input = $("#input");
   const value = input.value;
-  console.log("start call api::", value);
+  log("start call api::", value);
   const url = `https://api.github.com/users/${value}`;
 
   const data = await fetch(url);
@@ -21,19 +27,33 @@ const clickOrder = async () => {
     name.innerHTML = "No user found";
   }
 };
+// debounce function
 const debounce = (fn, delay) => {
   delay = delay || 0;
   let timerId;
   return function () {
-    console.log("timerId previous at::", timerId);
-    let context = this;
-    let args = arguments;
+    log("timerId previous at::", timerId);
     if (timerId) {
       clearTimeout(timerId);
       timerId = null;
     }
     timerId = setTimeout(() => {
-      return fn.apply(context, args);
+      return fn.apply(this, arguments);
     }, delay);
+  };
+};
+
+// init throttle function
+const throttle = (fn, delay) => {
+  delay = delay || 0;
+  let lastCall = 0;
+  return function () {
+    log("lastCall previous at::", lastCall);
+    const now = new Date().getTime();
+    if (now - lastCall < delay) {
+      return;
+    }
+    lastCall = now;
+    return fn.apply(this, arguments);
   };
 };
